2ecd82d3d671990d682a7833b8a8e9ac4e3b8221
usonkrap <usonkrap@gmail.com>
2018-11-19 22:00:41 +0900

    Loading state

--- src/App.js
+++ src/App.js
@@ -4,31 +4,28 @@ import Movie from './Movie'
 
 class App extends Component {
   state = {
-    movies: [
-      {
-        title: "Trainspotting",
-        image: "https://images-na.ssl-images-amazon.com/images/I/51hapGfPc9L.jpg"
-      },
-      {
-        title: "Matrix",
-        image: "https://images-na.ssl-images-amazon.com/images/I/51FhdGyJ6DL.jpg"
-      },
-      {
-        title: "Star Wars",
-        image: "https://images-na.ssl-images-amazon.com/images/I/81P3lDJbjCL._SY550_.jpg"
-      },
-      {
-        title: "Inception",
-        image: "https://images-na.ssl-images-amazon.com/images/I/51bDICODpZL.jpg"
-      }
-    ]
   }
   // https://reactjs.org/docs/react-component.html
   componentDidMount(){
     setTimeout(() => {
       this.setState({
         movies: [
-          ...this.state.movies,
+          {
+            title: "Trainspotting",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51hapGfPc9L.jpg"
+          },
+          {
+            title: "Matrix",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51FhdGyJ6DL.jpg"
+          },
+          {
+            title: "Star Wars",
+            image: "https://images-na.ssl-images-amazon.com/images/I/81P3lDJbjCL._SY550_.jpg"
+          },
+          {
+            title: "Inception",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51bDICODpZL.jpg"
+          },
           {
             title: "Being John Malkovich",
             image: "https://images-na.ssl-images-amazon.com/images/I/5192PHQF6GL.jpg"
@@ -38,12 +35,16 @@ class App extends Component {
     }, 5000);
   }
 
+  _renderMovies = () => {
+    return this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.image} key={index} />
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        {this.state.movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.image} key={index} />
-        })}
+        {this.state.movies ? this._renderMovies() : "Loading..."}
       </div>
     );
   }
