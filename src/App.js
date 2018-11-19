8d2b859bc65dd90937b328f9a643b6434e5abda1
usonkrap <usonkrap@gmail.com>
2018-11-19 21:07:38 +0900

    Lists with .map

--- src/App.js
+++ src/App.js
@@ -2,29 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie'
 
-const movieTitle = [
-  "trainspotting",
-  "Matrix",
-  "StarWars",
-  "Inception"
+const movies = [
+  {
+    title: "trainspotting",
+    image: "https://images-na.ssl-images-amazon.com/images/I/51hapGfPc9L.jpg"
+  },
+  {
+    title: "Matrix",
+    image: "https://images-na.ssl-images-amazon.com/images/I/51FhdGyJ6DL.jpg"
+  },
+  {
+    title: "StarWars",
+    image: "https://images-na.ssl-images-amazon.com/images/I/81P3lDJbjCL._SY550_.jpg"
+  },
+  {
+    title: "Inception",
+    image: "https://images-na.ssl-images-amazon.com/images/I/51bDICODpZL.jpg"
+  }
 ]
-
-const movieImage = [
-  "https://images-na.ssl-images-amazon.com/images/I/51hapGfPc9L.jpg",
-  "https://images-na.ssl-images-amazon.com/images/I/51FhdGyJ6DL.jpg",
-  "https://images-na.ssl-images-amazon.com/images/I/81P3lDJbjCL._SY550_.jpg",
-  "https://images-na.ssl-images-amazon.com/images/I/51bDICODpZL.jpg"
-]
-
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie title={movieTitle[0]} poster={movieImage[0]} />
-        <Movie title={movieTitle[1]} poster={movieImage[1]} />
-        <Movie title={movieTitle[2]} poster={movieImage[2]} />
-        <Movie title={movieTitle[3]} poster={movieImage[3]} />
+        {movies.map(movie => {
+          return <Movie title={movie.title} poster={movie.image} />
+        })}
       </div>
     );
   }
