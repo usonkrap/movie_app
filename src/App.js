118d9f6daad876b8fe84c1ad8567774efff06796
usonkrap <usonkrap@gmail.com>
2018-11-19 21:51:41 +0900

    Component state

--- src/App.js
+++ src/App.js
@@ -2,30 +2,46 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie'
 
-const movies = [
-  {
-    title: "trainspotting",
-    image: "https://images-na.ssl-images-amazon.com/images/I/51hapGfPc9L.jpg"
-  },
-  {
-    title: "Matrix",
-    image: "https://images-na.ssl-images-amazon.com/images/I/51FhdGyJ6DL.jpg"
-  },
-  {
-    title: "StarWars",
-    image: "https://images-na.ssl-images-amazon.com/images/I/81P3lDJbjCL._SY550_.jpg"
-  },
-  {
-    title: "Inception",
-    image: "https://images-na.ssl-images-amazon.com/images/I/51bDICODpZL.jpg"
-  }
-]
-
 class App extends Component {
+  state = {
+    movies: [
+      {
+        title: "Trainspotting",
+        image: "https://images-na.ssl-images-amazon.com/images/I/51hapGfPc9L.jpg"
+      },
+      {
+        title: "Matrix",
+        image: "https://images-na.ssl-images-amazon.com/images/I/51FhdGyJ6DL.jpg"
+      },
+      {
+        title: "Star Wars",
+        image: "https://images-na.ssl-images-amazon.com/images/I/81P3lDJbjCL._SY550_.jpg"
+      },
+      {
+        title: "Inception",
+        image: "https://images-na.ssl-images-amazon.com/images/I/51bDICODpZL.jpg"
+      }
+    ]
+  }
+  // https://reactjs.org/docs/react-component.html
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({
+        movies: [
+          ...this.state.movies,
+          {
+            title: "Being John Malkovich",
+            image: "https://images-na.ssl-images-amazon.com/images/I/5192PHQF6GL.jpg"
+          }
+        ]
+      });
+    }, 5000);
+  }
+
   render() {
     return (
       <div className="App">
-        {movies.map((movie, index) => {
+        {this.state.movies.map((movie, index) => {
           return <Movie title={movie.title} poster={movie.image} key={index} />
         })}
       </div>
