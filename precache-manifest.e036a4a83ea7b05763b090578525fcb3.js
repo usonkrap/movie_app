697ea26530b11c0d9912b92b064781ab91d83b4d
usonkrap <usonkrap@gmail.com>
2018-11-23 11:06:44 +0900

    Updates

---
+++ precache-manifest.e036a4a83ea7b05763b090578525fcb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "5ac704bc37b814680f6c",
+    "url": "/movie_app/static/js/main.5ac704bc.chunk.js"
+  },
+  {
+    "revision": "d0c28ac72245ff7380f5",
+    "url": "/movie_app/static/js/1.d0c28ac7.chunk.js"
+  },
+  {
+    "revision": "5ac704bc37b814680f6c",
+    "url": "/movie_app/static/css/main.0bef144e.chunk.css"
+  },
+  {
+    "revision": "1af57bab2ccabc0741f7773d684df704",
+    "url": "/movie_app/index.html"
+  }
+];
